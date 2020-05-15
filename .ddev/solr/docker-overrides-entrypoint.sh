836e281bdbffb0f554851872022adc397c5dff30
Rafael Kähm <rafael.kaehm@dkd.de>
2020-05-15 16:40:29 +0200

    [TASK] running solr with UID:GID as HOST user on OsX

--- .ddev/solr/docker-overrides-entrypoint.sh
+++ .ddev/solr/docker-overrides-entrypoint.sh
@@ -4,19 +4,19 @@ set -e
 
 echo '$@ = '"$@"
 
-everrideSolrUnixUidAndGidAndSetPermissions ()
+overrideSolrUnixUidAndGidAndSetPermissions ()
 {
   if [[ -v SOLR_OVERRIDE_UID ]] && grep -E -q '^[0-9]+$' <<<"${SOLR_OVERRIDE_UID:-}"; then
-    usermod -u "$SOLR_OVERRIDE_UID" solr
+    usermod --non-unique --uid "$SOLR_OVERRIDE_UID" solr
   fi
 
   if [[ -v SOLR_OVERRIDE_GID ]] && grep -E -q '^[0-9]+$' <<<"${SOLR_OVERRIDE_GID:-}"; then
-    groupmod -g "$SOLR_OVERRIDE_GID" solr
+    groupmod --non-unique --gid "$SOLR_OVERRIDE_GID" solr
   fi
 
   chown -R solr:solr /var/solr
 }
 
-everrideSolrUnixUidAndGidAndSetPermissions
+overrideSolrUnixUidAndGidAndSetPermissions
 
 su solr --preserve-environment -c 'docker-entrypoint.sh '"$@"
