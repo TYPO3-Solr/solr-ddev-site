20f301dd8b6fcafb1718d8a84cfe3f49cfce0333
Rafael Kähm <rafael.kaehm@dkd.de>
2022-10-14 12:34:40 +0200

    [TASK] Make solr-ddev-site PHP 8.1 compatible

--- packages/apache_solr_for_typo3_sitepackage/ext_localconf.php
+++ packages/apache_solr_for_typo3_sitepackage/ext_localconf.php
@@ -6,6 +6,11 @@ defined('TYPO3_MODE') || die();
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['apache_solr_for_typo3_sitepackage'] = 'EXT:apache_solr_for_typo3_sitepackage/Configuration/RTE/Default.yaml';
 
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
+  @import \'EXT:apache_solr_for_typo3_sitepackage/Configuration/TypoScript/constants.typoscript\'
+');
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
-  module.tx_form.settings.yamlConfigurations.100 = EXT:apache_solr_for_typo3_sitepackage/Configuration/Form/CustomFormSetup.yaml
+  @import \'EXT:apache_solr_for_typo3_sitepackage/Configuration/TypoScript/setup.typoscript\'
 ');
