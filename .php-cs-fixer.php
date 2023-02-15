3843dcf6de8fbb3d9a0d7e433f46d08afb97d853
Rafael Kähm <rafael.kaehm@dkd.de>
2023-02-15 10:35:10 +0100

    [TASK] Use PSR2 import_order rule for typo/coding-standards

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,7 +1,18 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
-$config->getFinder()
+$config
+    ->addRules(
+        [
+            'ordered_imports' => [
+                'imports_order' => ['class', 'function', 'const'],
+                'sort_algorithm' => 'alpha'
+            ]
+        ],
+    )
+    ->getFinder()
     ->exclude([
         '.Build'
     ])
