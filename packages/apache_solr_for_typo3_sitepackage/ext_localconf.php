f2327d9ca126d34a1efc5232e4969646b2586af9
Rafael Kähm <rafael.kaehm@dkd.de>
2023-11-17 13:46:27 +0100

    [TASK] Prepare release-11.2.x for ELTS: Use typo3/cms-introduction data directly

--- packages/apache_solr_for_typo3_sitepackage/ext_localconf.php
+++ packages/apache_solr_for_typo3_sitepackage/ext_localconf.php
@@ -9,3 +9,11 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['apache_solr_for_typo3_sitepackage
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
   module.tx_form.settings.yamlConfigurations.100 = EXT:apache_solr_for_typo3_sitepackage/Configuration/Form/CustomFormSetup.yaml
 ');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
+  @import \'EXT:apache_solr_for_typo3_sitepackage/Configuration/TypoScript/constants.typoscript\'
+');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
+  @import \'EXT:apache_solr_for_typo3_sitepackage/Configuration/TypoScript/setup.typoscript\'
+');
