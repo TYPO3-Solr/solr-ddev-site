3e7facab5bd1b758f94ae07c59ab2584dcf5da62
Rafael Kähm <rafael.kaehm@dkd.de>
2024-09-06 16:34:19 +0200

    [FEATURE] Allow run EXT:solr major versions in parallel
    This change allows to run multiple major versions of EXT:solr in parallel.
    Each major branch has its own DDEV project name and requeres different project in IDE or an own folder.
    
    Only main branch will keep the original project name and does not require any change in your system

--- config/system/additional.php
+++ config/system/additional.php
@@ -6,8 +6,8 @@ use TYPO3\CMS\Core\Utility\ArrayUtility;
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = E_WARNING | E_USER_ERROR | E_USER_WARNING | E_USER_NOTICE | E_RECOVERABLE_ERROR | E_DEPRECATED | E_USER_DEPRECATED;
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = E_WARNING | E_RECOVERABLE_ERROR | E_DEPRECATED | E_USER_DEPRECATED;
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern']  = 'localhost|solr-ddev-site.ddev.site';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['solr']['sites'][1]['domains'][]  = 'solr-ddev-site.ddev.site';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern'] = 'localhost|' . getenv('DDEV_HOSTNAME');
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['solr']['sites'][1]['domains'][] = getenv('DDEV_HOSTNAME');
 
 // Temporary fix:
 $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_sendmail_command'] = '/usr/local/bin/mailhog sendmail test@example.org --smtp-addr 127.0.0.1:1025 -bs';
