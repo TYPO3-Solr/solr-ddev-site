18b78bc28b9d732e692d17a3e92b02b6978bd242
Rafael Kähm <rafael.kaehm@dkd.de>
2025-12-08 21:51:30 +0100

    [TASK] Switch to pure TYPO3 Site-Sets for EXT:solr 13.1.x+
    This commit switches the solr-ddev-site to pure TYPO3 Site-Set mode.
    Set all EXT:solr* versions as 13.1.99 to prevent conflicts on `ddev solr:enable <addon-or-demo>` command and cross-ext version constraints like:
    ```
    apache-solr-for-typo3/solrfal 13.0 requires apache-solr-for-typo3/solr ^13.0.2 <14
    ```
    
    Updates import data to avoid running upgrade wizards.

--- packages/introduction_news/ext_localconf.php
+++ packages/introduction_news/ext_localconf.php
@@ -1,10 +1,2 @@
 <?php
 defined('TYPO3') || die();
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
-  @import \'EXT:introduction_news/Configuration/TypoScript/constants.typoscript\'
-');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
-  @import \'EXT:introduction_news/Configuration/TypoScript/setup.typoscript\'
-');
--- packages/introduction_solrfal/ext_localconf.php
+++ packages/introduction_solrfal/ext_localconf.php
@@ -1,14 +1,2 @@
 <?php
 defined('TYPO3') || die();
-
-/***************
- * Add default RTE configuration
- */
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
-  @import \'EXT:introduction_solrfal/Configuration/TypoScript/constants.typoscript\'
-');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
-  @import \'EXT:introduction_solrfal/Configuration/TypoScript/setup.typoscript\'
-');
