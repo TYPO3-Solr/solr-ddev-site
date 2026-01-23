8a3c5f4e4a05a7c125055b24945993a261eb894b
Rafael Kähm <rafael.kaehm@dkd.de>
2026-01-23 18:15:50 +0100

    [TASK] Setup rector for TYPO3 14
    <Description, wrap the lines after 100 characters manually>
    
    Refs: #<issue number>

--- rector.php
+++ rector.php
@@ -24,8 +24,8 @@ return RectorConfig::configure()
     ->withSets([
         Typo3SetList::CODE_QUALITY,
         Typo3SetList::GENERAL,
-        Typo3LevelSetList::UP_TO_TYPO3_13,
-        PHPUnitSetList::PHPUNIT_100,
+        Typo3LevelSetList::UP_TO_TYPO3_14,
+        PHPUnitSetList::PHPUNIT_110,
     ])
     # To have a better analysis from PHPStan, we teach it here some more things
     ->withPHPStanConfigs([
@@ -33,12 +33,6 @@ return RectorConfig::configure()
     ])
     ->withRules([
         AddVoidReturnTypeWhereNoReturnRector::class,
-        ConvertImplicitVariablesToExplicitGlobalsRector::class,
-    ])
-    ->withConfiguredRule(ExtEmConfRector::class, [
-        ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.1.0-8.3.99',
-        ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '12.4.3-12.4.99',
-        ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => []
     ])
     ->withImportNames()
     ->withSkip([
